53ddbc87c7a0817d4304af8b699f26bb65b76b55
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2023-11-22 20:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 //your code here
-/* const openModalButton = document.querySelector('#open-modal');
+ const openModalButton = document.querySelector('#open-modal');
 const modal = document.querySelector('#newton-modal');
 
 openModalButton.addEventListener('click', () => {
   modal.classList.add('show');
 });
- */
-
-/* const closeModalButton = document.querySelector('.modal-footer button');
+ 
+ const closeModalButton = document.querySelector('.modal-footer button');
 
 closeModalButton.addEventListener('click', () => {
   modal.classList.remove('show');
-}); */
+}); 
 
