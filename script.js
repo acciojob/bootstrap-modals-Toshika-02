24b02f096fa4344b26fabd1a6e8b789a7958bea6
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2023-11-22 20:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+const openModalButton = document.querySelector('#open-modal');
+const modal = document.querySelector('#newton-modal');
+
+openModalButton.addEventListener('click', () => {
+  modal.classList.add('show');
+});
+
+
+const closeModalButton = document.querySelector('.modal-footer button');
+
+closeModalButton.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
+
